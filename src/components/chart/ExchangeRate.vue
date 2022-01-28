5f1873ae4af7bac571db771594e61459d5448dfc
JunYoung <ko5642027@naver.com>
2022-01-28 14:50:48 +0900

    환율 정보 차트 등록

---
+++ src/components/chart/ExchangeRate.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="chart">
+    <canvas id="ExchageRatesChart" width="400" height="400"></canvas>
+  </div>
+</template>
+
+<script>
+import { Chart, registerables } from "chart.js";
+import zommPlugin from "chartjs-plugin-zoom";
+Chart.register(...registerables, zommPlugin);
+import jsonData from "../../../tempData/exchagerates.json";
+import Vue from "vue";
+
+export default Vue.extend({
+  mounted() {
+    const ctx = document.getElementById("ExchageRatesChart");
+
+    const config = {
+      type: "line",
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: "환율 정보",
+            data: [],
+            pointRadius: 0,
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: false,
+          },
+        },
+        scrollingMode: "all",
+        plugins: {
+          zoom: {
+            zoom: {
+              wheel: {
+                enabled: true,
+              },
+              pinch: {
+                enabled: true,
+              },
+            },
+          },
+        },
+      },
+    };
+
+    jsonData.forEach((e) => {
+      config.data.datasets[0].data.push(Number(e.KRW));
+      config.data.labels.push(e.date);
+    });
+
+    new Chart(ctx, config);
+  },
+});
+</script>
+
+<style scoped>
+.chart {
+  float: left;
+  padding: 2rem;
+  border: 1px solid red;
+  width: 500px;
+}
+</style>
